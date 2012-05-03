4c0709311191ec1deb20aaa2af13bd0feb1b4bc6
hzel <hazel.tan@orangeandbronze.com>
2012-05-03 12:33:04 +0800

    Added BootStrap and Subject Domain
    Signed-off-by: hzel <hazel.tan@orangeandbronze.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,20 @@
-class BootStrap {
+import com.onb.registrationsystem.Role
+import com.onb.registrationsystem.User
+import com.onb.registrationsystem.UserRole
 
+class BootStrap {
     def init = { servletContext ->
+    def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+      def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+      def testUser = new User(username: 'me', enabled: true, password: 'password')
+      testUser.save(flush: true)
+
+      UserRole.create testUser, adminRole, true
+
+      assert User.count() == 1
+      assert Role.count() == 2
+      assert UserRole.count() == 1
     }
     def destroy = {
     }
---
+++ grails-app/domain/com/onb/registrationsystem/Subject.groovy
@@ -0,0 +1,7 @@
+package com.onb.registrationsystem
+
+class Subject {
+
+    static constraints = {
+    }
+}
